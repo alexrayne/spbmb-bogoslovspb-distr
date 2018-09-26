6ac2266d262628457a23a1ce2cbceeb4d734b61e
alexraynepe196 <alexraynepe196@gmail.com>
2018-09-26 10:31:52 +0300

    import SFI menu, CSS
    Imported from sfi.ru main menu and css source

---
+++ DistributionPackages/SpbMB.BogoslovSpb/Resources/Public/Frontend/sfi/scss/gulp-install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo apt install -y npm
+sudo npm install -g gulp 
+sudo npm install -g gulp-sass gulp-sass-glob
+sudo npm install -g gulp-autoprefixer browser-sync gulp-uglify gulp-concat gulp-concat-css gulp-cssnano
